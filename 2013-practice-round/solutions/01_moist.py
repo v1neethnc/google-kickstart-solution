b72a3fc856fe564acb729c0f3df99fbc0c4d9973
Vineeth <ncvineeth97@gmail.com>
2022-10-04 00:19:08 -0400

    Start 2013 Practice Round

---
+++ 2013-practice-round/solutions/01_moist.py
@@ -0,0 +1,27 @@
+# Google Kickstart: Practice Round, 2013
+# Title: Moist (4 pts, 6 pts)
+# https://github.com/v1neethnc/google-kickstart-solutions
+
+# Run the code from the 2013-practice-round folder using:
+# python solutions/01_moist.py < /files/01_sample_input1.txt
+# python solutions/01_moist.py < /files/01_test_input1.txt
+# python solutions/01_moist.py < /files/01_test_input2.txt
+# Compare the console outputs with the ones in the corresponding output files
+
+n = int(input())
+ctr = 0
+while ctr < n:
+	v = int(input())
+	ctr += 1
+	names = []
+	temp_ctr, res = 0, 0
+	while temp_ctr < v:
+		ind = 0
+		nm = input()
+		while len(names) > ind and nm > names[ind]:
+			ind += 1
+		names.insert(ind, nm)
+		if len(names) > 1 and ind != temp_ctr:
+			res += 1
+		temp_ctr += 1
+	print(f"Case #{ctr}: {res}")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # google-kickstart-solution
-My solutions to Google Kickstart. Ongoing project
+My solutions to Google Kickstart. Ongoing project. Working my way through the archives for now.
